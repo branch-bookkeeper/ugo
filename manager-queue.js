86be8204785157f312b389a732f07e6584a7c806
Massimiliano Cannarozzo <maxcanna@gmail.com>
2017-09-17 18:50:41 +0200

    Add queue manager

---
+++ manager-queue.js
@@ -0,0 +1,44 @@
+const redis = require('./redis');
+const postal = require('postal');
+const prefix = 'booking';
+
+class QueueManager {
+    static addItem(queue, item) {
+        return redis.push(`${prefix}:${queue}`, item)
+            .then(data => {
+                postal.publish({
+                    channel: 'queue',
+                    topic: 'item.add',
+                    data: {
+                        queue,
+                        item,
+                    },
+                });
+                return data;
+            });
+    }
+
+    static removeItem(queue, item) {
+        return redis.lrem(`${prefix}:${queue}`, item)
+            .then(() => {
+                postal.publish({
+                    channel: 'queue',
+                    topic: 'item.remove',
+                    data: {
+                        queue,
+                        item,
+                    },
+                });
+            });
+    }
+
+    static getItems(queue) {
+        return redis.lrange(`${prefix}:${queue}`);
+    }
+
+    static enabled() {
+        return redis.enabled();
+    }
+}
+
+module.exports = QueueManager;
