aa94674cf5402c1b6a49ff00ab60b84c3c50c27d
Massimiliano Cannarozzo <maxcanna@gmail.com>
2017-09-22 22:22:06 +0200

    Add installation manager

---
+++ manager-installation.js
@@ -0,0 +1,100 @@
+const redis = require('./redis');
+const postal = require('postal');
+const Github = require('./github');
+const prefix = 'installation';
+const infoPrefix = 'info';
+
+class QueueManager {
+    static getInstallationId(owner) {
+        return redis.hget(prefix, owner);
+    }
+
+    static setInstallationId(owner, installationId) {
+        if (redis.enabled()) {
+            return redis.hset(prefix, `${owner}`, installationId)
+                .then(() => {
+                    postal.publish({
+                        channel: 'installation',
+                        topic: 'id.set',
+                        data: {
+                            owner,
+                            installationId,
+                        },
+                    });
+                });
+        }
+        return Promise.resolve();
+    }
+
+    static deleteInstallationId(owner) {
+        if (redis.enabled()) {
+            return redis.hdel(prefix, owner)
+                .then(() => {
+                    postal.publish({
+                        channel: 'installation',
+                        topic: 'id.delete',
+                        data: {
+                            owner,
+                        },
+                    });
+                });
+        }
+        return Promise.resolve();
+    }
+
+    static getInstallationInfo(token, installationId) {
+        if (redis.enabled()) {
+            return redis.get(`${prefix}:${infoPrefix}:${installationId}:${token}`)
+                .then(installationInfo => {
+                    if (!installationInfo) {
+                        return QueueManager._getInstallationInfoFromGithub(token, installationId);
+                    }
+                    return installationInfo;
+                });
+        }
+        return QueueManager._getInstallationInfoFromGithub(token, installationId);
+    }
+
+    static deleteInstallationInfos(installationId) {
+        if (redis.enabled()) {
+            return redis.keys(`${prefix}:${infoPrefix}:${installationId}`)
+                .then(keys => Promise.all(keys.map(redis.del)))
+                .then(() => {
+                    postal.publish({
+                        channel: 'installation',
+                        topic: 'info.delete',
+                        data: {
+                            installationId,
+                        },
+                    });
+                });
+        }
+        return Promise.resolve();
+    }
+
+    static _getInstallationInfoFromGithub(token, installationId) {
+        return Github.getInstallationInfo(token, installationId)
+            .then(installationInfo => QueueManager._setInstallationInfo(installationId, installationInfo, token));
+    }
+
+    static _setInstallationInfo(installationId, installationInfo, token) {
+        if (redis.enabled() && installationInfo) {
+            redis.set(`${prefix}:info:${installationId}:${token}`, installationInfo, 86400)
+                .then(() => {
+                    postal.publish({
+                        channel: 'installation',
+                        topic: 'info.set',
+                        data: {
+                            installationId,
+                            installationInfo,
+                            token,
+                        },
+                    });
+                })
+                .then(() => installationInfo);
+        }
+        return installationInfo;
+    }
+}
+
+module.exports = QueueManager;
