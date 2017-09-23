2e285d2155cd5df9a394f599c149e41eba9d9e92
Gabriele Genta <gabriele.genta@gmail.com>
2017-09-23 20:09:12 +0200

    WIP

--- redis.js
+++ redis.js
@@ -1,156 +1,163 @@
+const {
+    map,
+    fromPairs,
+    compose,
+    forEach,
+    replace,
+} = require('ramda');
 const redisLib = require('redis');
 const async = require('async');
 const logger = require('./logger');
 const EventEmitter = require('events');
-let redisClient;
 
-if (process.env['REDIS_URL']) {
-    redisClient = redisLib.createClient(
-        process.env['REDIS_URL'],
-        {
-            prefix: process.env['NODE_ENV'] ? process.env['NODE_ENV'] + ':' : '',
-        }
-    );
-}
+const { REDIS_URL, NODE_ENV } = process.env;
 
-const rejectIfNotConnected = (resolve, reject) => {
-    if (!process.env['REDIS_URL'] || !redisClient || !redisClient.ready) {
-        return Promise.reject(new Error('queue not connected'));
-    }
+const REDIS_CLIENT_METHODS = [
+    'lrem',
+    'rpush',
+    'set',
+    'hset',
+    'del',
+    'hdel',
+    'get',
+    'hget',
+    'lrange',
+    'keys',
+];
 
-    return new Promise(resolve, reject);
+const promisify = fun => function (...args) {
+    return new Promise((resolve, reject) => (
+        fun.apply(this, [...args, (error, result) => error ? reject(error) : resolve(result)])
+    ));
 };
 
-const redis = Object.assign({
+const jsonParse = data => JSON.parse(data);
+
+const createRedisClient = (redisUrl, nodeEnv = null) => {
+    const client = redisLib.createClient(
+        redisUrl,
+        {
+            prefix: nodeEnv ? nodeEnv + ':' : '',
+        }
+    );
+
+    // Promisify every method we are going to use
+    forEach(method => client[method] = promisify(client[method]), REDIS_CLIENT_METHODS);
+
+    return client;
+};
+
+const createNotConnectedRedisClient = () => {
+    const alwaysFail = () => Promise.reject(new Error('queue not connected'));
+
+    return {
+        ...compose(
+            fromPairs,
+            map(method => [method, alwaysFail])
+        )(REDIS_CLIENT_METHODS),
+        on: (event, callback) => event === 'unavailable' && callback(),
+    };
+};
+
+const redisClient = REDIS_URL
+    ? createRedisClient(REDIS_URL, NODE_ENV)
+    : createNotConnectedRedisClient();
+
+const redis = {
     enabled() {
-        return process.env['REDIS_URL'] !== undefined;
+        process.env.REDIS_URL !== undefined;
     },
     lrem(key, data) {
-        return rejectIfNotConnected((resolve, reject) => {
-            redisClient.lrem(key, 0, JSON.stringify(data), (err, reply) => {
-                redis.emit('lrem', { data: data, key: key });
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(JSON.parse(reply));
-                }
-            });
-        });
+        redis.emit('lrem', { data: data, key: key });
+
+        return redisClient.lrem(key, 0)
+            .then(jsonParse);
     },
     push(key, data) {
-        return rejectIfNotConnected((resolve, reject) => {
-            if (key && data) {
-                redisClient.rpush(key, JSON.stringify(data), (e, d) => e ? reject(e) : resolve(d));
-            }
-        });
+        if (key && data) {
+            return redisClient.rpush(key, JSON.stringify(data));
+        }
+
+        return Promise.reject('You must specify key and data');
     },
     set(key, data, ttl) {
-        return rejectIfNotConnected((resolve, reject) => {
-            if (key && data) {
-                const callback = (e, d) => e ? reject(e) : resolve(d);
-                const jsonData = JSON.stringify(data);
+        if (key && data) {
+            const jsonData = JSON.stringify(data);
 
-                if (ttl) {
-                    redisClient.set(key, jsonData, 'EX', ttl || Number.MAX_SAFE_INTEGER, callback);
-                } else {
-                    redisClient.set(key, jsonData, callback);
-                }
+            if (ttl) {
+                return redisClient.set(key, jsonData, 'EX', ttl);
+            } else {
+                return redisClient.set(key, jsonData);
             }
-        });
+        }
+
+        return Promise.reject('You must specify key and data');
     },
     hset(key, field, data) {
-        return rejectIfNotConnected((resolve, reject) => {
-            if (key && field && data) {
-                redisClient.hset(key, field, JSON.stringify(data), (e, d) => e ? reject(e) : resolve(d));
-            }
-        });
+        if (key && field && data) {
+            return redisClient.hset(key, field, JSON.stringify(data));
+        }
+
+        return Promise.reject('You must specify key, field and data');
     },
     del(key) {
-        return rejectIfNotConnected((resolve, reject) => {
-            if (key) {
-                redisClient.del(key, (e, d) => e ? reject(e) : resolve(d));
-            }
-        });
+        if (key) {
+            return redisClient.del(key);
+        }
+
+        return Promise.reject('You must specify key');
     },
     hdel(key, field) {
-        return rejectIfNotConnected((resolve, reject) => {
-            if (key && field) {
-                redisClient.hdel(key, field, (e, d) => e ? reject(e) : resolve(d));
-            }
-        });
+        if (key && field) {
+            return redisClient.hdel(key, field);
+        }
+
+        return Promise.reject('You must specify key and field');
     },
     get(key) {
-        return rejectIfNotConnected((resolve, reject) => {
-            if (key) {
-                redisClient.get(key, (err, data) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve(JSON.parse(data));
-                    }
-                });
-            }
-        });
+        if (key) {
+            return redisClient.get(key)
+                .then(jsonParse);
+        }
+
+        return Promise.reject('You must specify key');
     },
     hget(key, field) {
-        return rejectIfNotConnected((resolve, reject) => {
-            if (key && field) {
-                redisClient.hget(key, field, (err, data) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve(JSON.parse(data));
-                    }
-                });
-            }
-        });
+        if (key && field) {
+            return redisClient.hget(key, field)
+                .then(jsonParse);
+        }
+
+        return Promise.reject('You must specify key and field');
     },
     lrange(key, numberOfItems) {
-        return rejectIfNotConnected((resolve, reject) => {
-            if (numberOfItems) {
-                numberOfItems -= 1;
-            } else {
-                numberOfItems = Number.MAX_SAFE_INTEGER;
-            }
+        if (numberOfItems) {
+            numberOfItems -= 1;
+        } else {
+            numberOfItems = Number.MAX_SAFE_INTEGER;
+        }
 
-            redisClient.lrange(key, 0, numberOfItems, (err, reply) => {
-                if (!err) {
-                    try {
-                        resolve(reply.map(JSON.parse));
-                    } catch (e) {
-                        reject(e);
-                    }
-                } else {
-                    reject(err);
-                }
-            });
-        });
+        return redisClient.lrange(key, 0, numberOfItems)
+            .then(map(jsonParse));
     },
     keys(pattern) {
-        return rejectIfNotConnected((resolve, reject) => {
-            redisClient.keys(`${redisClient.options.prefix}${pattern}:*`, (err, rows) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(rows.map(row => row.replace(redisClient.options.prefix, '')));
-                }
-            });
-        });
+        return redisClient.keys(`${redisClient.options.prefix}${pattern}:*`)
+            .then(map(replace(redisClient.options.prefix, '')));
     },
-}, EventEmitter.prototype);
+    ...EventEmitter.prototype,
+};
 
-if (process.env['REDIS_URL'] && redisClient) {
-    redisClient.on('error', error => {
-        logger.error(error);
-        redis.emit('error', error);
-    });
-    redisClient.on('ready', () => {
-        logger.info('Connected to redis');
-        redis.emit('ready');
-    });
-} else {
+redisClient.on('error', error => {
+    logger.error(error);
+    redis.emit('error', error);
+});
+redisClient.on('ready', () => {
+    logger.info('Connected to redis');
+    redis.emit('ready');
+});
+redisClient.on('unavailable', () => {
     logger.info('redis parameters missing, skipping connection');
     redis.emit('unavailable');
-}
+});
 
 module.exports = redis;
