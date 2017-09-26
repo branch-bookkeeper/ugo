befbaffc0a18edad0dcd7612fa9f5879e30a7efc
Gabriele Genta <gabriele.genta@gmail.com>
2017-09-26 10:46:53 +0200

    Trigger event when private key is not available

--- application-event-handler.js
+++ application-event-handler.js
@@ -17,6 +17,11 @@ const downloadPrivateKey = () => {
                     },
                 });
             }));
+    } else {
+        postal.publish({
+            channel: 'github',
+            topic: 'key.not_available',
+        });
     }
 };
 
