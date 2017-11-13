297913252554bdab82a6f4712593a21b83f44af0
Massimiliano Cannarozzo <maxcanna@gmail.com>
2017-11-13 22:25:44 +0100

    Remove helper

--- helpers/queue-helpers.js
+++
@@ -1,9 +0,0 @@
-const unpackQueueName = (queueName) => {
-    const [owner, repo, branch] = queueName.split(':');
-
-    return { owner, repo, branch };
-};
-
-module.exports = {
-    unpackQueueName,
-};
