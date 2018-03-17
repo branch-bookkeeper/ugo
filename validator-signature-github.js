3fa0becffb1ee82701830534c7f029ddcaad28cd
Massimiliano Cannarozzo <maxcanna@gmail.com>
2018-03-17 13:48:25 +0100

    Optimize checks

--- validator-signature-github.js
+++ validator-signature-github.js
@@ -11,17 +11,12 @@ const validator = (req, res, next) => {
     }
 
     const headerSignature = req.get('X-Hub-Signature');
-
-    if (!headerSignature || headerSignature.indexOf('sha1=') !== 0) {
-        return next(createError.Unauthorized('Unauthorized'));
-    }
-
     const hmac = crypto.createHmac('sha1', token);
     hmac.update(Buffer.from(JSON.stringify(req.body)), 'utf-8');
 
     const computedSignature = `sha1=${hmac.digest('hex')}`;
 
-    if (headerSignature !== computedSignature) {
+    if (!headerSignature || headerSignature !== computedSignature) {
         return next(createError.Unauthorized('Unauthorized'));
     }
 
