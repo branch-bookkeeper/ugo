e56a2c7dd2cf283b2301d0dbd6b6110b5186a1b5
Massimiliano Cannarozzo <maxcanna@gmail.com>
2018-04-14 20:08:43 +0200

    Use nyc

--- test.sh
+++ test.sh
@@ -17,15 +17,18 @@ fi
 echo
 printf "\033[33mTesting with mongo\n\033[0m"
 
-node_modules/.bin/babel-istanbul cover --report clover node_modules/.bin/_mocha -- test/test* --ui tdd --reporter dot --timeout 10000
+nyc --reporter=none mocha test/test* --ui tdd --timeout 2000
 
-mv coverage/coverage.{json,mongo.json}
+mv .nyc_output{,-old}
 
 echo
 printf "\033[33mTesting without mongo\n\033[0m"
 
 unset MONGO_URL
 
-node_modules/.bin/babel-istanbul cover --report clover node_modules/.bin/_mocha -- test/test* --ui tdd --reporter dot --timeout 10000
+nyc --reporter=none mocha test/test* --ui tdd --timeout 2000
 
-node_modules/.bin/istanbul report --report clover
+cp .nyc_output-old/* .nyc_output/
+rm -rf .nyc_output-old
+
+nyc report --reporter=clover
