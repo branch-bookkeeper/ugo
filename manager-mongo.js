b9142dbfc41d8ec0afb8e04124acde6eb5b99518
Massimiliano Cannarozzo <maxcanna@gmail.com>
2019-01-27 15:24:01 +0100

    Improve database availability check

--- manager-mongo.js
+++ manager-mongo.js
@@ -1,15 +1,27 @@
+let database;
 const MongoClient = require('mongodb').MongoClient;
 const logger = require('./logger');
 const { isNil } = require('ramda');
 const { env: { MONGO_URL } } = process;
 const getDb = new Promise(resolve => {
     if (MONGO_URL) {
+        if (!isNil(database)) {
+            return resolve(database);
+        }
         MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
             .then(c => c.db())
             .then(db => {
                 logger.info('Connected to mongo');
+                database = db;
                 resolve(db);
+            })
+            .catch(e => {
+                logger.error('Error connectiong to mongo', e);
+                resolve({});
             });
+    } else {
+        logger.error('Missing MONGO_URL env var');
+        resolve({});
     }
 });
 
@@ -19,7 +31,7 @@ class MongoManager {
     }
 
     static enabled() {
-        return !isNil(MONGO_URL);
+        return !isNil(MONGO_URL) && !isNil(database);
     }
 
     static reset() {
