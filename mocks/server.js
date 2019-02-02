a44522b9f09ea77dced837cf354cdc03826ecdf7
Massimiliano Cannarozzo <maxcanna@gmail.com>
2019-02-02 12:42:29 +0100

    Mock check suites route

--- mocks/server.js
+++ mocks/server.js
@@ -1,4 +1,5 @@
 const statusSuccess = require('../test/fixtures/status.success');
+const suitesSuccess = require('../test/fixtures/suites.success');
 const repositories = require('../test/fixtures/installation.info');
 const pullRequest = require('../test/fixtures/pull_request.info');
 const user = require('../test/fixtures/token.info');
@@ -10,6 +11,7 @@ const accessTokens = {
 module.exports = () => {
     return {
         status: statusSuccess,
+        suites: suitesSuccess,
         accessTokens,
         repositories,
         pullRequest,
